26b3e22d9dae6d63e117577090a7d750727bad62
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-08-04 17:58:58 +0300

    System

--- first_project/exe/Main.hs
+++ first_project/exe/Main.hs
@@ -1,11 +1,15 @@
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
 import Sqeq (sq)
-
+import System.Exit (die)
 main :: IO ()
 main = do
-  [as, bs, cs] <- getArgs
-  let
-    a = read as
-    b = read bs
-    c = read cs
-  putStrLn $ "Solutions: " ++ show(sq a b c)
+  prog <- getProgName
+  args <- getArgs
+  case args of
+    [as, bs, cs] ->
+      let 
+        a = read as
+        b = read bs
+        c = read cs
+      in putStrLn $ "Solutions: " ++ show(sq a b c)
+    _ -> die $ "Usage:" ++ prog ++ " NUMBER NUMBER NUMBER"
