5a0b712a2b2c4fc99d2fc7d0cadc111ee8371781
Alex <pechenkin.aa@phystech.edu>
2020-08-21 14:19:32 +0300

    final

---
+++ first_project/.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.0.1.0/build/sq-test/autogen/Paths_sq_test.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_sq_test (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/progervpn/github/haskell_education/first_project/.stack-work/install/x86_64-linux-tinfo6/81c16d674ac60006090b74fa58385535fa6984460ac6f81d3d3bdd91e4a6db7f/8.8.3/bin"
+libdir     = "/home/progervpn/github/haskell_education/first_project/.stack-work/install/x86_64-linux-tinfo6/81c16d674ac60006090b74fa58385535fa6984460ac6f81d3d3bdd91e4a6db7f/8.8.3/lib/x86_64-linux-ghc-8.8.3/sq-test-0-Fl8Tga94Rmw23v33uaMA69-sq-test"
+dynlibdir  = "/home/progervpn/github/haskell_education/first_project/.stack-work/install/x86_64-linux-tinfo6/81c16d674ac60006090b74fa58385535fa6984460ac6f81d3d3bdd91e4a6db7f/8.8.3/lib/x86_64-linux-ghc-8.8.3"
+datadir    = "/home/progervpn/github/haskell_education/first_project/.stack-work/install/x86_64-linux-tinfo6/81c16d674ac60006090b74fa58385535fa6984460ac6f81d3d3bdd91e4a6db7f/8.8.3/share/x86_64-linux-ghc-8.8.3/sq-test-0"
+libexecdir = "/home/progervpn/github/haskell_education/first_project/.stack-work/install/x86_64-linux-tinfo6/81c16d674ac60006090b74fa58385535fa6984460ac6f81d3d3bdd91e4a6db7f/8.8.3/libexec/x86_64-linux-ghc-8.8.3/sq-test-0"
+sysconfdir = "/home/progervpn/github/haskell_education/first_project/.stack-work/install/x86_64-linux-tinfo6/81c16d674ac60006090b74fa58385535fa6984460ac6f81d3d3bdd91e4a6db7f/8.8.3/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "sq_test_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "sq_test_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "sq_test_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "sq_test_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "sq_test_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "sq_test_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
--- first_project/exe/Main.hs
+++ first_project/exe/Main.hs
@@ -1,9 +1,40 @@
 import System.Environment (getArgs, getProgName)
 import Sqeq (sq)
 import System.Exit (die)
+import Options.Applicative (helper, info, execParser, header, fullDesc, progDesc, argument, auto, metavar, help, value, option, long, short, Parser)
+import Control.Applicative ((<**>))
+
+data Options = Options{a,b,c :: Double, lang :: Language}
+
+data Language = Ru | En
+  deriving (Read, Show)
 main :: IO ()
 main = do
-  prog <- getProgName
+  Options {a = a, b = b, c = c, lang = lang} <- execParser parserInfo
+  let 
+    solution = 
+      case lang of
+        En -> "Solutions"
+        Ru -> "Решения"
+  putStrLn $ solution ++ ": " ++ show(sq a b c)
+
+parserInfo = 
+  info 
+    (parser <**> helper)
+    ( fullDesc <> header "Solve square equation")
+
+parser :: Parser Options
+parser =
+  Options 
+  <$> argument auto ( metavar "A" <> help "x^2 term")
+  <*> argument auto (metavar "B" <> help "x term")
+  <*> argument auto (metavar "C" <> help "free term") 
+  <*> option auto (metavar "language" <> help "Language (En, Ru)" <> value En <> long "Language" <> short 'l')
+
+  
+
+
+{-  prog <- getProgName
   args <- getArgs
   case args of
     [as, bs, cs] ->
@@ -12,4 +43,4 @@ main = do
         b = read bs
         c = read cs
       in putStrLn $ "Solutions: " ++ show(sq a b c)
-    _ -> die $ "Usage:" ++ prog ++ " NUMBER NUMBER NUMBER"
+    _ -> die $ "Usage:" ++ prog ++ " NUMBER NUMBER NUMBER"-}
